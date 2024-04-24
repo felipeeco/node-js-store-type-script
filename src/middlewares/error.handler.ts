49658b2f4c109e54b424d2653af441bbd9eab750
felipeeco <contacto@felipemoreno.co>
2024-04-23 21:54:25 -0500

    feat(*) build debugger enviroment

--- src/middlewares/error.handler.ts
+++ src/middlewares/error.handler.ts
@@ -14,6 +14,7 @@ export function errorHandler(
   error: Error,
   req: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) {
   res.status(500).json({
