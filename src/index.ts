454f9323efe7aef1a76993a01e735b8edd5e21e5
felipeeco <contacto@felipemoreno.co>
2024-05-01 22:20:08 -0500

    fix(index.ts) try

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(cors());
 
 // Routes
 app.get('/api', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express!');
+  res.send('Hello, TypeScript Express! VERCEL');
 });
 
 // Router
