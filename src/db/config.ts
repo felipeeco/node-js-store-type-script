8a9d5dc0fa0008f160df34134a0689d9c2330ab6
Luis Felipe Moreno Rondon <luisfelipe.moreno.contractor@bbva.com>
2024-08-15 18:21:10 -0500

    feat(*): create migrations

---
+++ src/db/config.ts
@@ -0,0 +1,16 @@
+import { config } from '../../config/config';
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+module.exports = {
+ development: {
+  url: URI,
+  dialect: 'postgres',
+ },
+ production: {
+  url: URI,
+  dialect: 'postgres',
+ }
+};
